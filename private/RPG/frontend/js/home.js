79101097b72d60e8f48b8a877af407f15a817a19
shimojit <0liberty4life@gmail.com>
2024-08-20 13:12:44 +0900

    directory changed.

---
+++ private/RPG/frontend/js/home.js
@@ -0,0 +1,5 @@
+const btnIntroduction = document.querySelector('.introduction');
+const btnContact = document.querySelector('.contact');
+const btnBlog = document.querySelector('.blog');
+const btnAll = document.querySelectorAll('button');
+const dialog = document.querySelector('.dialog');
--- private/RPG/js/home.js
+++
@@ -1,3 +0,0 @@
-const btnIntroduction = document.getquerySelector('.introduction');
-const btnContact = document.getquerySelector('.contact');
-const btnBlog = document.getquerySelector('.blog');
